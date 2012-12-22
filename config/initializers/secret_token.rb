0038f14726a7203d24cdf041bb0817fe380cd9a0
tyrel86 <tyrelrichey@gmail.com>
2012-12-21 18:10:53 -0700

    Base rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloverLeaf::Application.config.secret_token = '66bc5788baa43117b6fc54f7043faa7775c82d53f82d5824213a824d52f162179a73fbaf97ee20a36e84e3933f9a29ceccad7eb35ee897741f2b5163f7a985b4'
